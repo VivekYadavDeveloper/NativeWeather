9f867d1c08fe695f4e37135ba30c33e56f3e0e76
vivek <vk662967@gmail.com>
2024-11-09 00:07:39 +0530

    Full Design Complete And Bug in Image Load

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.runtime.livedata)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,8 +68,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    val retrofitVersion ="2.11.0"
-    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    val retrofitVersion = "2.11.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+//    implementation("io.coil-kt.coil3:coil-compose:3.0.1")
+//    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.1")
 
 }
--- app/src/main/java/com/create/nativeweather/WeatherPage.kt
+++ app/src/main/java/com/create/nativeweather/WeatherPage.kt
@@ -1,12 +1,18 @@
 package com.create.nativeweather
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,9 +26,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+
 import com.create.nativeweather.api.NetworkResponse
+import com.create.nativeweather.api.WeatherModel
 
 
 @Composable
@@ -63,7 +76,8 @@ fun WeatherPage(name: String, modifier: Modifier, viewModel: WeatherViewModel) {
 
         when (val result = weatherResult.value) {
             is NetworkResponse.Success -> {
-                Text(text = result.data.toString())
+                WeatherDetails(data = result.data, Modifier)
+//                Text(text = result.data.toString())
 
             }
 
@@ -83,4 +97,95 @@ fun WeatherPage(name: String, modifier: Modifier, viewModel: WeatherViewModel) {
     }
 }
 
-//TODO: Composable Design Panding
+//TODO: Composable Design Pending
+
+
+@Composable
+fun WeatherDetails(data: WeatherModel, modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom
+        ) {
+
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location Icon",
+                modifier = Modifier.size(40.dp)
+            )
+            Text(text = data.location.name, fontSize = 24.sp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = data.location.country, fontSize = 17.sp, color = Color.Gray)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Text(text = data.current.condition.text, fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "${data.current.temp_c}°C", fontSize = 64.sp)
+
+        AsyncImage(
+            modifier = Modifier.size(160.dp),
+            model = "https:${data.current.condition.icon}".replace("64x64", "128x128"),
+            contentDescription = "Weather Icon",
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(R.drawable.baseline_broken_image_24), // replace with your actual placeholder
+            error = painterResource(R.drawable.baseline_error_24)
+        )
+
+//        Text(text = data.current.condition.text, fontSize = 64.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Card {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyValue(value = data.current.humidity, key = "Humidity")
+                    WeatherKeyValue(value = data.current.wind_kph, key = "Wind Speed")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyValue(value = data.current.uv, key = "UV")
+                    WeatherKeyValue(value = data.current.precip_mm, key = "Participation")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyValue(
+                        value = data.location.localtime.split(" ")[1],
+                        key = "Local Time"
+                    )
+                    WeatherKeyValue(
+                        value = data.location.localtime.split(" ")[0],
+                        key = "Local Date"
+                    )
+                }
+            }
+        }
+
+
+    }
+}
+
+
+@Composable
+fun WeatherKeyValue(key: String, value: String) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text(text = key, fontSize = 16.sp, fontWeight = FontWeight.SemiBold, color = Color.Gray)
+    }
+}
